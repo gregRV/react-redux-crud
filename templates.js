df099e451e0334bcfe880023853dff3690fb32fc
Greg Varias <gregvarias@gmail.com>
2016-08-18 13:13:25 -0700

    function creates crud actiontypes given a resource name

--- templates.js
+++ templates.js
@@ -11,5 +11,21 @@ const resourceClass = (resource) => {
     `
 }
 
+const resourceActionTypes = (resource) => {
+  const keys = ['FETCH_', 'CREATE_', 'UPDATE_', 'DELETE_'];
+  const status = ['_START', '_SUCCESS', '_ERROR'];
+  const resourceCaps = resource.toUpperCase();
+  let actionTypes = {};
+  
+  keys.forEach((key) => {
+   status.forEach((status) => {
+     actionTypes[`${key}${resourceCaps}${status}`] = `${key}${resourceCaps}${status}`;
+   }); 
+  });
+  
+  return actionTypes;
+}
+
 console.log(resourceClass('User'));
+console.log(resourceActionTypes('User'));
 module.exports = resourceClass;
