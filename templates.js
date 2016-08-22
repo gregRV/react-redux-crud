557458075b3735bdaf4e917fb67e9dd13379b2fa
Greg Varias <gregvarias@gmail.com>
2016-08-22 15:42:35 -0700

    Generator provide templates for basic actions and reducers

--- templates.js
+++ templates.js
@@ -16,11 +16,11 @@ const resourceActionTypes = (resource) => {
   const status = ['_START', '_SUCCESS', '_ERROR'];
   const resourceCaps = resource.toUpperCase();
   let actionTypes = {};
-  
+
   keys.forEach((key) => {
    status.forEach((status) => {
      actionTypes[`${key}${resourceCaps}${status}`] = `${key}${resourceCaps}${status}`;
-   }); 
+   });
   });
 
   return Object.keys(actionTypes).reduce((memo, curr) => {
@@ -40,70 +40,69 @@ const resourceActions = (resource) => {
   const capitalized = resource[0].toUpperCase() + resource.substr(1);
 
   return Object.keys(keys).reduce((memo, key) => {
-    memo +=
-    `export function ${key}${capitalized}() {
-      return dispatch => {
-        dispatch({type: ${key.toUpperCase()}_${resource.toUpperCase()}_START});
-        api${keys[key]}${capitalized}()
-          .then((data) => { dispatch({type: ${key.toUpperCase()}_${resource.toUpperCase()}_SUCCESS, payload: data}) });${EOL}
-      `
+    memo += `export ${key}${capitalized} = () => {
+  return dispatch => {
+    dispatch({type: ${key.toUpperCase()}_${resource.toUpperCase()}_START});
+    // api${keys[key]}${capitalized}()
+      .then((data) => { dispatch({type: ${key.toUpperCase()}_${resource.toUpperCase()}_SUCCESS, payload: data}) });
+  }
+}\n${EOL}`
     return memo;
   }, '');
 }
 
 const resourceReducer = (resource) => {
   const caps = resource.toUpperCase();
-  
-  return `
-    import {
-      FETCH_${caps}_START,
-      FETCH_${caps}_SUCCESS,
-      FETCH_${caps}_ERROR,
-      CREATE_${caps}_START,
-      CREATE_${caps}_SUCCESS,
-      CREATE_${caps}_ERROR,
-      UPDATE_${caps}_START,
-      UPDATE_${caps}_SUCCESS,
-      UPDATE_${caps}_ERROR,
-      DELETE_${caps}_START,
-      DELETE_${caps}_SUCCESS,
-      DELETE_${caps}_ERROR
-    } from ./${resource}-actions;
 
-    function ${resource}(state=[], action) {
-      switch(action.type) {
-        case FETCH_${caps}_START:
-          return state;
-        case FETCH_${caps}_SUCCESS:
-          return state;
-        case FETCH_${caps}_ERROR:
-          return state;
-        
-        case CREATE_${caps}_START:
-          return state;
-        case CREATE_${caps}_SUCCESS:
-          return state;
-        case CREATE_${caps}_ERROR:
-          return state;
-          
-        case UPDATE_${caps}_START:
-          return state;
-        case UPDATE_${caps}_SUCCESS:
-          return state;
-        case UPDATE_${caps}_ERROR:
-          return state;
-        
-        case DELETE_${caps}_START:
-          return state;
-        case DELETE_${caps}_SUCCESS:
-          return state;
-        case DELETE_${caps}_ERROR:
-          return state;
+  return `import {
+  FETCH_${caps}_START,
+  FETCH_${caps}_SUCCESS,
+  FETCH_${caps}_ERROR,
+  CREATE_${caps}_START,
+  CREATE_${caps}_SUCCESS,
+  CREATE_${caps}_ERROR,
+  UPDATE_${caps}_START,
+  UPDATE_${caps}_SUCCESS,
+  UPDATE_${caps}_ERROR,
+  DELETE_${caps}_START,
+  DELETE_${caps}_SUCCESS,
+  DELETE_${caps}_ERROR
+} from ./${resource}-actions;
 
-        default:
-          return state;
-      }
-    }
+export default ${resource} = (state=[], action) => {
+  switch(action.type) {
+    case FETCH_${caps}_START:
+      return state;
+    case FETCH_${caps}_SUCCESS:
+      return state;
+    case FETCH_${caps}_ERROR:
+      return state;
+
+    case CREATE_${caps}_START:
+      return state;
+    case CREATE_${caps}_SUCCESS:
+      return state;
+    case CREATE_${caps}_ERROR:
+      return state;
+
+    case UPDATE_${caps}_START:
+      return state;
+    case UPDATE_${caps}_SUCCESS:
+      return state;
+    case UPDATE_${caps}_ERROR:
+      return state;
+
+    case DELETE_${caps}_START:
+      return state;
+    case DELETE_${caps}_SUCCESS:
+      return state;
+    case DELETE_${caps}_ERROR:
+      return state;
+
+    default:
+      return state;
+  }
+}
   `;
 }
 
@@ -111,4 +110,11 @@ const resourceReducer = (resource) => {
 // console.log(resourceActionTypes('User'));
 // console.log(resourceActions('user'));
 // console.log(resourceReducer('user'));
-module.exports = resourceClass;
+module.exports = {
+  resource: {
+    class: resourceClass,
+    actionTypes: resourceActionTypes,
+    actions: resourceActions,
+    reducer: resourceReducer
+  }
+};
